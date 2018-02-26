0c30f1f5edeb6808d5cf1179ee253d3b90b6ddf9
Nolan Byrnes <nolanehs@gmail.com>
2018-02-26 10:57:54 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,7 +5,7 @@ class SchoolClassesController <ApplicationController
   end
 
   def new
-    @school_class = SchoolClass.create(post_params(:title, :room_number))
+    @school_class = SchoolClass.create(school_class_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
 
