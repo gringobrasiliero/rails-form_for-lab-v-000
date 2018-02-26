6da9672dad7049dc52451ba289800524b9333646
Nolan Byrnes <nolanehs@gmail.com>
2018-02-26 09:42:38 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,23 @@
 class SchoolClassesController <ApplicationController
 
+  def index
+    @school_class = Coupon.all
+  end
+
+  def new
+  end
+
+  def show
+      @student = Coupon.find(params[:id])
+    end
+
+  def create
+    @student = Coupon.new
+    @student.coupon_code = params[:coupon_code]
+    @student.store = params[:store]
+    @student.save
+    redirect_to coupon_path(@student)
+  end
+
+
 end
