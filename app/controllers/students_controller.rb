1952dad59749b213837d14156dc46019f16fc5cc
Nolan Byrnes <nolanehs@gmail.com>
2018-02-26 10:51:25 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
     end
 
   def create
-    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+    @student = Student.new(post_params))
     @student.save
     redirect_to student_path(@student)
   end
