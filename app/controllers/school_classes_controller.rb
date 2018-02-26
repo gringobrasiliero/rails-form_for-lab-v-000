b8e3c348e90e31c289951e44abf0b87b0da162e2
Nolan Byrnes <nolanehs@gmail.com>
2018-02-26 10:47:24 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -14,12 +14,16 @@ class SchoolClassesController <ApplicationController
     end
 
   def create
-    @school_class = SchoolClass.new
-    @school_class.title = params[:title]
-    @school_class.room_number = params[:room_number]
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
+
+def update
+  @school_class = SchoolClass.find(params[:id])
+  @school_class.update(params.require(:school_class).permit(:title, :room_number))
+  redirect_to school_class_path(@school_class)
+end
 
 private
 
