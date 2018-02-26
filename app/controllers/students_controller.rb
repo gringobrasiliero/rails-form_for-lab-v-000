c873620673b2c73ea4c839ee7fab973ca93d265e
Nolan Byrnes <nolanehs@gmail.com>
2018-02-26 11:40:23 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,6 +17,10 @@ class StudentsController < ApplicationController
     redirect_to student_path(@student)
   end
 
+def edit
+  @student = Student.find(params[:id])
+end
+
 def update
 @student = Student.find(params[:id])
 @student.update(student_params(:first_name, :last_name))
